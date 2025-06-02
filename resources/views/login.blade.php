ea5f9c4c500cde749776dabcbca0be4c88fe675a
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-02 21:44:56 +0700

    create login view page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,43 +1,45 @@
 @extends('templates.template')
 
 @section('content')
-    <div class="row h-100">
-        <div class="col-lg-5 col-12">
-            <div id="auth-left">
-                <div class="auth-logo">
-                    <a href="{{ url('') }}" class="fs-1 fw-bold">
-                        SJ Arsip
-                    </a>
+    <div id="auth">
+        <div class="row h-100">
+            <div class="col-lg-5 col-12">
+                <div id="auth-left">
+                    <div class="auth-logo">
+                        <a href="{{ url('') }}" class="fs-1 fw-bold">
+                            SJ Arsip
+                        </a>
+                    </div>
+                    <h1 class="auth-title">Log in.</h1>
+                    <p class="auth-subtitle mb-5">Silahkan login terlebih dahulu.</p>
+
+                    <form action="index.html">
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="text" class="form-control form-control-xl" placeholder="Username">
+                            <div class="form-control-icon">
+                                <i class="bi bi-person"></i>
+                            </div>
+                        </div>
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="password" class="form-control form-control-xl" placeholder="Password">
+                            <div class="form-control-icon">
+                                <i class="bi bi-shield-lock"></i>
+                            </div>
+                        </div>
+                        <div class="form-check form-check-lg d-flex align-items-end">
+                            <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label text-gray-600" for="flexCheckDefault">
+                                Tampilkan kata sandi
+                            </label>
+                        </div>
+                        <a href="{{ url('/admin/dashboard') }}" class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</a>
+                    </form>
                 </div>
-                <h1 class="auth-title">Log in.</h1>
-                <p class="auth-subtitle mb-5">Silahkan login terlebih dahulu.</p>
-
-                <form action="index.html">
-                    <div class="form-group position-relative has-icon-left mb-4">
-                        <input type="text" class="form-control form-control-xl" placeholder="Username">
-                        <div class="form-control-icon">
-                            <i class="bi bi-person"></i>
-                        </div>
-                    </div>
-                    <div class="form-group position-relative has-icon-left mb-4">
-                        <input type="password" class="form-control form-control-xl" placeholder="Password">
-                        <div class="form-control-icon">
-                            <i class="bi bi-shield-lock"></i>
-                        </div>
-                    </div>
-                    <div class="form-check form-check-lg d-flex align-items-end">
-                        <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
-                        <label class="form-check-label text-gray-600" for="flexCheckDefault">
-                            Tampilkan kata sandi
-                        </label>
-                    </div>
-                    <a href="{{ url('/admin/dashboard') }}" class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</a>
-                </form>
             </div>
-        </div>
-        <div class="col-lg-7 d-none d-lg-block">
-            <div id="auth-right">
+            <div class="col-lg-7 d-none d-lg-block">
+                <div id="auth-right">
 
+                </div>
             </div>
         </div>
     </div>
