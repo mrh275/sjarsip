9d25044885d49af916a72ec52f2600f1b42c3610
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-02 20:25:54 +0700

    modify user fields that can be filled

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,10 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
+        'username',
         'name',
-        'email',
         'password',
+        'role',
     ];
 
     /**
@@ -41,7 +42,6 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
