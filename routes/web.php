481205deac05bc3b92b5e50cedcb52e71d2c122e
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-02 21:43:59 +0700

    added new route for dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ArsipController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LoginController::class, 'index']);
+Route::get('/admin/dashboard', [ArsipController::class, 'index']);
