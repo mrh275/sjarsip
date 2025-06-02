a7d941673aa0d32d54caa2b78af58e8db6df48bc
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-02 21:44:15 +0700

    create index method for dashboard

--- app/Http/Controllers/ArsipController.php
+++ app/Http/Controllers/ArsipController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class ArsipController extends Controller
 {
-    //
+    public function index()
+    {
+
+        $data = [
+            'title' => 'Dashboard',
+            'sidebar' => 'active',
+        ];
+
+        return view('admin.dashboard', $data);
+    }
 }
