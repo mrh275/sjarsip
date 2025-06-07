83ace6cffc7d1fadd08989dae2813126761bc585
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-07 16:31:37 +0700

    create new view for tambah arsip

---
+++ resources/views/admin/tambah-arsip.blade.php
@@ -0,0 +1,44 @@
+@extends('templates.template')
+
+@section('content')
+    <div id="app">
+        @include('templates.sidebar')
+        <div id="main" class="layout-navbar navbar-fixed">
+            @include('templates.navbar')
+
+            <div id="main-content">
+                <div class="page-heading">
+                    <h3>Tambah Arsip</h3>
+                </div>
+                <div class="page-content">
+                    <section class="row">
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="card">
+                                        <div class="card-body px-4 py-4-5">
+                                            <div class="row">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+
+            <footer>
+                <div class="footer clearfix mb-0 text-muted">
+                    <div class="float-start">
+                        <p>2023 &copy; Mazer</p>
+                    </div>
+                    <div class="float-end">
+                        <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
+                            by <a href="https://saugi.me">Saugi</a></p>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+@endsection
