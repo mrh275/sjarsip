4d6249e296a44f368e7c1d1f919b067e3388daa2
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-07 17:33:09 +0700

    add new route for data arsip page

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [LoginController::class, 'index']);
 Route::get('/admin/dashboard', [ArsipController::class, 'index']);
 Route::get('/admin/tambah-surat', [ArsipController::class, 'tambahArsip']);
+Route::get('/admin/data-arsip', [ArsipController::class, 'dataArsip']);
