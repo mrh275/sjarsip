f9e0f5ff1b90bc3974c5837acedca5fbf7daf431
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-07 20:19:51 +0700

    add new route for report page

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,4 @@ Route::get('/', [LoginController::class, 'index']);
 Route::get('/admin/dashboard', [ArsipController::class, 'index']);
 Route::get('/admin/tambah-surat', [ArsipController::class, 'tambahArsip']);
 Route::get('/admin/data-arsip', [ArsipController::class, 'dataArsip']);
+Route::get('/admin/laporan', [ArsipController::class, 'laporan']);
