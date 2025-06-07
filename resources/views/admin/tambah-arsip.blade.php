d214b99704d463dad33f47b5b978f6a31fd958ca
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-07 17:16:06 +0700

    create page view for add new surat

--- resources/views/admin/tambah-arsip.blade.php
+++ resources/views/admin/tambah-arsip.blade.php
@@ -18,6 +18,33 @@
                                     <div class="card">
                                         <div class="card-body px-4 py-4-5">
                                             <div class="row">
+                                                <h6>Masukan rincian data surat yang akan ditambahkan</h6>
+
+                                                <div class="col-12 col-lg-6 my-4">
+                                                    <form action="{{ url('admin/tambah-surat') }}" method="POST" id="tambah-surat">
+                                                        @csrf
+                                                        <div class="mb-3 form-group">
+                                                            <label for="no_surat" class="form-label">Nomor Surat</label>
+                                                            <input type="text" class="form-control" id="no_surat" name="no_surat" required>
+                                                        </div>
+                                                        <div class="mb-3 form-group">
+                                                            <label for="no_surat" class="form-label">Customer</label>
+                                                            <input type="text" class="form-control" id="no_surat" name="no_surat" required>
+                                                        </div>
+                                                        <div class="mb-3 form-group">
+                                                            <label for="no_surat" class="form-label">Tanggal Surat</label>
+                                                            <input type="date" class="form-control" id="no_surat" name="no_surat" required>
+                                                        </div>
+                                                        <div class="mb-3 form-group d-flex justify-content-end">
+                                                            <button type="submit" class="btn btn-primary">Simpan</button>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                                <div class="col-12 col-lg-6 my-4">
+                                                    <p class="mb-2">Unggah file surat</p>
+                                                    <form action="" class="dropzone" id="unggah-surat" method="POST" enctype="multipart/form-data">
+                                                    </form>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
@@ -42,3 +69,33 @@
         </div>
     </div>
 @endsection
+
+@push('scripts')
+    <script>
+        Dropzone.options.unggahSurat = {
+            url: "{{ url('admin/unggah-surat') }}",
+            method: "post",
+            paramName: "file",
+            maxFilesize: 2, // MB
+            acceptedFiles: ".pdf,.jpg,.jpeg,.png",
+            addRemoveLinks: true,
+            headers: {
+                'X-CSRF-TOKEN': "{{ csrf_token() }}"
+            },
+            init: function() {
+                this.on("success", function(file, response) {
+                    console.log(response);
+                    if (response.status === 'success') {
+                        alert('File berhasil diunggah!');
+                    } else {
+                        alert('Gagal mengunggah file.');
+                    }
+                });
+                this.on("error", function(file, response) {
+                    console.error(response);
+                    alert('Terjadi kesalahan saat mengunggah file.');
+                });
+            }
+        }
+    </script>
+@endpush
