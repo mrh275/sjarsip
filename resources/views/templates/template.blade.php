0c0353b1bcd4ed18195e7462aea2191b20dc65d0
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-07 19:50:32 +0700

    remove navbar js assets

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -28,7 +28,6 @@
 
     @yield('content')
 
-    <script src="{{ asset('assets/static/js/components/dark.js') }}"></script>
     <script src="{{ asset('assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
     <script src="{{ asset('assets/compiled/js/app.js') }}"></script>
     <!-- Need: Apexcharts -->
