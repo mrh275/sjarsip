6305a2bd5eb4db95d733f6a554091f542cc8d7c9
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-07 16:29:54 +0700

    add new route for tambah arsip view

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LoginController::class, 'index']);
 Route::get('/admin/dashboard', [ArsipController::class, 'index']);
+Route::get('/admin/tambah-surat', [ArsipController::class, 'tambahArsip']);
