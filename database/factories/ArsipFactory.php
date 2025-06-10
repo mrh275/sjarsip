f0c422a36505997260823908ede9b6be76f9781e
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-10 21:57:58 +0700

    create factory for dummyt data

--- database/factories/ArsipFactory.php
+++ database/factories/ArsipFactory.php
@@ -17,7 +17,10 @@ class ArsipFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'no_surat_jalan' => $this->faker->unique()->numerify('SJ-#####'),
+            'customer' => $this->faker->company,
+            'tanggal_surat' => $this->faker->dateTimeBetween('-1 year', 'now'),
+            'kode_surat' => '#' . $this->faker->unique()->numberBetween(10000, 99999),
         ];
     }
 }
