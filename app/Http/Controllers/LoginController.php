ab96abe269e024232e84b362db3b720502dd4240
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-10 21:31:21 +0700

    create new method for handling login attempt

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -13,5 +14,26 @@ class LoginController extends Controller
             'sidebar' => 'login',
         ];
         return view('login', $data);
+    }
+
+    public function login(Request $request)
+    {
+
+        $credentials = [
+            'username' => $request->input('username'),
+            'password' => $request->input('password'),
+        ];
+
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            // Authentication passed...
+            return redirect()->to('/admin/dashboard')->with('success', 'Login successful');
+        } else {
+            return redirect()->back()->withErrors(['login' => 'Invalid credentials'])->withInput();
+        }
     }
 }
