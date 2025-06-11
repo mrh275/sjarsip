fc14d7be77c1848e43c7fe31c89b6f93b1d38ed4
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-11 08:15:36 +0700

    modified profile menu on navbar

--- resources/views/templates/navbar.blade.php
+++ resources/views/templates/navbar.blade.php
@@ -15,19 +15,19 @@
                     <a href="#" data-bs-toggle="dropdown" aria-expanded="false">
                         <div class="user-menu d-flex">
                             <div class="user-name text-end me-3">
-                                <h6 class="mb-0 text-gray-600">John Ducky</h6>
+                                <h6 class="mb-0 text-gray-600">{{ session('name') }}</h6>
                                 <p class="mb-0 text-sm text-gray-600">Administrator</p>
                             </div>
                             <div class="user-img d-flex align-items-center">
                                 <div class="avatar avatar-md">
-                                    <img src="./assets/compiled/jpg/1.jpg">
+                                    <img src="{{ asset('assets/compiled/jpg/1.jpg') }}">
                                 </div>
                             </div>
                         </div>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton" style="min-width: 11rem;">
                         <li>
-                            <h6 class="dropdown-header">Hello, John!</h6>
+                            <h6 class="dropdown-header">Hello, {{ session('name') }}!</h6>
                         </li>
                         <li><a class="dropdown-item" href="#"><i class="icon-mid bi bi-person me-2"></i> My
                                 Profile</a></li>
