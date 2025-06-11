6238261ba1a59862760d5cc03ce83ac79abf8eac
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-11 08:16:25 +0700

    modified form input, merge form upload and basic form

--- resources/views/admin/tambah-arsip.blade.php
+++ resources/views/admin/tambah-arsip.blade.php
@@ -17,12 +17,12 @@
                                 <div class="col-12">
                                     <div class="card">
                                         <div class="card-body px-4 py-4-5">
-                                            <div class="row">
-                                                <h6>Masukan rincian data surat yang akan ditambahkan</h6>
+                                            <form action="{{ url('admin/tambah-surat') }}" method="POST" id="tambah-surat" enctype="multipart/form-data">
+                                                <div class="row">
+                                                    <h6>Masukan rincian data surat yang akan ditambahkan</h6>
 
-                                                <div class="col-12 col-lg-6 my-4">
-                                                    <form action="{{ url('admin/tambah-surat') }}" method="POST" id="tambah-surat">
-                                                        @csrf
+                                                    @csrf
+                                                    <div class="col-12 col-lg-6 my-4">
                                                         <div class="mb-3 form-group">
                                                             <label for="no_surat_jalan" class="form-label">Nomor Surat</label>
                                                             <input type="text" class="form-control" id="no_surat_jalan" name="no_surat_jalan" required>
@@ -35,17 +35,18 @@
                                                             <label for="tanggal_surat" class="form-label">Tanggal Surat</label>
                                                             <input type="date" class="form-control" id="tanggal_surat" name="tanggal_surat" required>
                                                         </div>
+                                                    </div>
+                                                    <div class="col-lg-6 col-12 my-4">
+                                                        <div class="mb-3 form-group">
+                                                            <label for="unggah_surat" class="mb-3">Unggah file surat</label>
+                                                            <input type="file" id="unggah_surat" name="unggah_surat" class="image-preview-filepond">
+                                                        </div>
                                                         <div class="mb-3 form-group d-flex justify-content-end">
                                                             <button type="submit" class="btn btn-primary">Simpan</button>
                                                         </div>
-                                                    </form>
+                                                    </div>
                                                 </div>
-                                                <div class="col-12 col-lg-6 my-4">
-                                                    <p class="mb-2">Unggah file surat</p>
-                                                    <form action="" class="dropzone" id="unggah-surat" method="POST" enctype="multipart/form-data">
-                                                    </form>
-                                                </div>
-                                            </div>
+                                            </form>
                                         </div>
                                     </div>
                                 </div>
@@ -67,6 +68,16 @@
 @endsection
 
 @push('scripts')
+    <script src="{{ asset('assets') }}/extensions/filepond-plugin-file-validate-size/filepond-plugin-file-validate-size.min.js"></script>
+    <script src="{{ asset('assets') }}/extensions/filepond-plugin-file-validate-type/filepond-plugin-file-validate-type.min.js"></script>
+    <script src="{{ asset('assets') }}/extensions/filepond-plugin-image-crop/filepond-plugin-image-crop.min.js"></script>
+    <script src="{{ asset('assets') }}/extensions/filepond-plugin-image-exif-orientation/filepond-plugin-image-exif-orientation.min.js"></script>
+    <script src="{{ asset('assets') }}/extensions/filepond-plugin-image-filter/filepond-plugin-image-filter.min.js"></script>
+    <script src="{{ asset('assets') }}/extensions/filepond-plugin-image-preview/filepond-plugin-image-preview.min.js"></script>
+    <script src="{{ asset('assets') }}/extensions/filepond-plugin-image-resize/filepond-plugin-image-resize.min.js"></script>
+    <script src="{{ asset('assets') }}/extensions/filepond/filepond.js"></script>
+    <script src="{{ asset('assets') }}/extensions/toastify-js/src/toastify.js"></script>
+    <script src="{{ asset('assets') }}/static/js/pages/filepond.js"></script>
     <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
     <script>
         Dropzone.options.unggahSurat = {
