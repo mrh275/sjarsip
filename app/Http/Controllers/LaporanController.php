c2b64fd55a2679d0fac7107b84bb527c9da628af
Muhamad Ramdani Hidayatullah <muhamadramdani.tmkm@gmail.com>
2025-06-25 21:47:34 +0700

    create new controller for export laporan

---
+++ app/Http/Controllers/LaporanController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Arsip;
+use Barryvdh\DomPDF\Facade\Pdf as FacadePdf;
+use Barryvdh\DomPDF\PDF;
+use Illuminate\Http\Request;
+
+class LaporanController extends Controller
+{
+    public function cetakLaporan(Request $request)
+    {
+        if (!session()->has('username')) {
+            return redirect()->to('/')->with('error', 'You must be logged in to access this page.');
+        }
+
+        // Logic to generate PDF report
+        // This is a placeholder; you would implement your PDF generation logic here
+        $format = $request->input('format', 'pdf'); //Default to PDF if not specified
+        $source = $request->input('source', '1');
+        $bulan = $request->input('month', null);
+        $tahun = $request->input('year', 2024);
+        $arsip = [];
+        if ($source == '1') {
+            $arsip = Arsip::whereYear('tanggal_surat', $tahun)->get();
+        } else {
+            $arsip = Arsip::whereYear('tanggal_surat', $tahun)
+                ->whereMonth('tanggal_surat', $bulan)->get();
+        }
+        // dd($arsip);
+        $pdf = FacadePdf::loadView('admin.export-pdf', ['arsip' => $arsip, 'tahun' => $tahun, 'bulan' => $bulan]);
+
+        // 4. Download PDF
+        // Nama file PDF akan menjadi "arsip-2023-10.pdf" misalnya.
+        $namaFile = 'arsip-' . $tahun . '-' . str_pad($bulan, 2, '0', STR_PAD_LEFT) . '.pdf';
+        return $pdf->download($namaFile);
+    }
+}
