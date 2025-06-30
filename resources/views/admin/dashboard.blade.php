2e1d605d43813233ef5cf1d51e301d625506dfc0
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-30 21:03:05 +0700

    Modified card title

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -58,7 +58,7 @@
                                                     </div>
                                                 </div>
                                                 <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                                    <h6 class="text-muted font-semibold">Following</h6>
+                                                    <h6 class="text-muted font-semibold">User</h6>
                                                     <h6 class="font-extrabold mb-0">80.000</h6>
                                                 </div>
                                             </div>
