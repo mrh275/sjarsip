c3312e95cb384fb232c962b2be9e8e9d93d8506c
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-07-25 21:28:32 +0700

    change static data for customer with data from db

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -42,7 +42,7 @@
                                                 </div>
                                                 <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                                     <h6 class="text-muted font-semibold">Customer</h6>
-                                                    <h6 class="font-extrabold mb-0">38</h6>
+                                                    <h6 class="font-extrabold mb-0">{{ $customer }}</h6>
                                                 </div>
                                             </div>
                                         </div>
